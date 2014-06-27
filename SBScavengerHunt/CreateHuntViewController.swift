35c9147b19c0c0fdce3da6a0f69fbfd1d377cfae
Jordan Clifton <jfc1254@gmail.com>
2014-06-27 10:57:26 -0400

    initial setup with advertising and some view setup

---
+++ SBScavengerHunt/CreateHuntViewController.swift
@@ -0,0 +1,63 @@
+//
+//  CreateHuntViewController.swift
+//  SBScavengerHunt
+//
+//  Created by JFClifton on 6/27/14.
+//  Copyright (c) 2014 smashingboxes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MultipeerConnectivity
+
+class CreateHuntViewController : UIViewController, MCNearbyServiceAdvertiserDelegate, UIActionSheetDelegate {
+    
+    var localPeerID = MCPeerID(displayName:UIDevice.currentDevice().name)
+    var advertiser : MCNearbyServiceAdvertiser = MCNearbyServiceAdvertiser()
+    
+    init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
+        // Custom init
+        super.init(nibName:nibName, bundle:nibBundle)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        advertiser = MCNearbyServiceAdvertiser(peer:localPeerID, discoveryInfo:nil, serviceType:"SB-Hunt")
+        advertiser.delegate = self;
+        advertiser.startAdvertisingPeer()
+    }
+    
+    func advertiser(advertiser: MCNearbyServiceAdvertiser!, didReceiveInvitationFromPeer peerID: MCPeerID!, withContext context: NSData!, invitationHandler: ((Bool, MCSession!) -> Void)!)
+    {
+        var actionSheet = UIActionSheet()
+        actionSheet.delegate = self
+        actionSheet.title = "Received Invitation from \(peerID.displayName)"
+        actionSheet.addButtonWithTitle("Reject")
+        actionSheet.addButtonWithTitle("Block")
+        actionSheet.addButtonWithTitle("Accept")
+        actionSheet.cancelButtonIndex = 0;
+        actionSheet.showInView(self.view)
+    
+    }
+    
+    func actionSheet(__actionSheet: UIActionSheet!,
+        clickedButtonAtIndex buttonIndex: Int)
+    {
+        switch buttonIndex {
+        case 0:
+            println("Rejected")
+    
+        case 1:
+            println("Blocked")
+        
+        case 2:
+            println("Accepted")
+            
+        default:
+            println("nothing")
+        }
+    }
+    
+}
--- SBScavengerHunt/ViewController.swift
+++ SBScavengerHunt/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onCreatePressed(sender: AnyObject) {
+        var createVC = CreateHuntViewController(nibName:"CreateHuntViewController", bundle:nil)
+        self.navigationController.pushViewController(createVC, animated: true)
+        
+    }
 
 }
 
