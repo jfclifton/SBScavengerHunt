1459d266b6cf3f421abb3a888c76295b94c8f48a
David Sweetman <david@davidsweetman.com>
2014-06-27 10:21:19 -0400

    Fix data model

---
+++ SBScavengerHunt/Hunt.m
@@ -0,0 +1,18 @@
+//
+//  Hunt.m
+//  SBScavengerHunt
+//
+//  Created by David Sweetman on 6/27/14.
+//  Copyright (c) 2014 smashingboxes. All rights reserved.
+//
+
+#import "Hunt.h"
+
+
+@implementation Hunt
+
+@dynamic title;
+@dynamic huntDescription;
+@dynamic targets;
+
+@end
---
+++ SBScavengerHunt/HuntTarget.m
@@ -0,0 +1,23 @@
+//
+//  HuntTarget.m
+//  SBScavengerHunt
+//
+//  Created by David Sweetman on 6/27/14.
+//  Copyright (c) 2014 smashingboxes. All rights reserved.
+//
+
+#import "HuntTarget.h"
+#import "Hunt.h"
+
+
+@implementation HuntTarget
+
+@dynamic title;
+@dynamic descOutOfRange;
+@dynamic descFar;
+@dynamic descNear;
+@dynamic descImmediate;
+@dynamic par;
+@dynamic hunt;
+
+@end
