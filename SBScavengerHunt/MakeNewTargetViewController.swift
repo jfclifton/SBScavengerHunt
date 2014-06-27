eb146549c2b9a42d2a6b8a5f79ae62fd6ca6fa4e
David Sweetman <david@davidsweetman.com>
2014-06-27 10:49:16 -0400

    Begin hunt creation VCs

---
+++ SBScavengerHunt/MakeNewTargetViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MakeNewTargetView.swift
+//  SBScavengerHunt
+//
+//  Created by David Sweetman on 6/27/14.
+//  Copyright (c) 2014 smashingboxes. All rights reserved.
+//
+
+import UIKit
+
+class MakeNewTargetViewController: UIViewController {
+    
+    var moc : NSManagedObjectContext?
+    var hunt : Hunt?
+    
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
