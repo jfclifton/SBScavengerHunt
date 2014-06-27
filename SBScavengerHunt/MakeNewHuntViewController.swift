9996782406f4e30b4febc7bc20610d205a5584a4
David Sweetman <david@davidsweetman.com>
2014-06-27 11:05:23 -0400

    Progress on target creator vc

--- SBScavengerHunt/MakeNewHuntViewController.swift
+++ SBScavengerHunt/MakeNewHuntViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class MakeNewHuntViewController: UIViewController {
+class MakeNewHuntViewController: UIViewController,
+UITableViewDelegate, UITableViewDataSource {
     
     var moc : NSManagedObjectContext?
     var hunt : Hunt?
+    var targets : Array<HuntTarget>?
     @IBOutlet var titleTextField : UITextField
     @IBOutlet var descriptionTextField : UITextField
     @IBOutlet var addNewTargetBtn : UIButton
@@ -29,6 +31,11 @@ class MakeNewHuntViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        updateTargets()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -38,10 +45,29 @@ class MakeNewHuntViewController: UIViewController {
         let targetVC = MakeNewTargetViewController(nibName: "MakeNewTargetViewController", bundle: nil)
         targetVC.moc = moc
         targetVC.hunt = hunt
-        
+        self.navigationController.pushViewController(targetVC, animated: true)
     }
     
     @IBAction func doneButtonPressed(sender : AnyObject) {
-        
+        // Move to brodcast to local players
+    }
+    
+    //MARK: UITableViewDataSource
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return hunt!.targets.count
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "ReuseID")
+        cell.text = "\(targets![indexPath.row].title)"
+        return cell
+    }
+    
+    //MARK: UITableViewDelegate
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) { }
+    
+    func updateTargets() {
+        targets = hunt!.targets.array as? Array<HuntTarget>
     }
 }
